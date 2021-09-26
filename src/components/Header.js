94c596162e101e516def2faef0aa90ec66660553
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-09-26 10:59:18 +1000

    Complete Header Right Part

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,6 +27,27 @@ function header() {
           />
           <SearchIcon className='h-12 p-4' />
         </div>
+        {/* Rght Section */}
+        <div className='flex text-white items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+          <div className='link'>
+            <p> Hello, Abhishekh Maharjan </p>
+            <p className='font-extrabold md:text-sm'> Account and Lists</p>
+          </div>
+          <div className='link'>
+            <p> Return </p>
+            <p className='font-extrabold md:text-sm'> And Orders</p>
+          </div>
+          <div className='link relative flex items-center'>
+            <span className='bg-yellow-400 rounded-full absolute top-0 right-0 md:right-10 h-4 w-4 text-center text-black font-bold'>
+              0
+            </span>
+            <ShoppingCartIcon className='h-10' />
+            <p className='hidden md:inline font-extrabold md:text-sm mt-2'>
+              {" "}
+              Basket
+            </p>
+          </div>
+        </div>
       </div>
       {/* Bottom Nav */}
     </header>
